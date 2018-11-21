7309efda36c10783665032dfc47a766a752961d2
Mitch Kelley <mitch.kelley@solo.io>
2018-11-21 10:16:18 -0500

    add mesh selection to ca config

--- cli/pkg/cmd/config/ca/root.go
+++ cli/pkg/cmd/config/ca/root.go
@@ -1,7 +1,11 @@
 package ca
 
 import (
+	"fmt"
+
 	"github.com/solo-io/supergloo/cli/pkg/cmd/options"
+	"github.com/solo-io/supergloo/cli/pkg/common"
+	"github.com/solo-io/supergloo/cli/pkg/meshutil"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +16,7 @@ func Cmd(opts *options.Options) *cobra.Command {
 		Short: `Update CA`,
 		Long:  `Update CA`,
 		Run: func(c *cobra.Command, args []string) {
+			configureCa(opts)
 		},
 	}
 
@@ -30,4 +35,36 @@ func Cmd(opts *options.Options) *cobra.Command {
 	flags.StringVar(&cOpts.Secret.Namespace, "secretnamespace", "", "namespace in which to store the secret")
 
 	return cmd
+}
+
+func configureCa(opts *options.Options) {
+	err := ensureFlags(opts)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("wip")
+	return
+}
+
+func ensureFlags(opts *options.Options) error {
+	oMeshRef := &(opts.Config.Ca).Mesh
+	// TODO(mitchdraft) - this block essentially gets a mesh resource ref if your cmd needs one
+	// It is very similar to logic in create/routing_rule.go
+	// Abstract so that it operates on a pointer to a Mesh ResourceRef, providing validation and optional interactive selection mode
+	if oMeshRef.Name == "" {
+		// Q(mitchdraft) do we want to prefilter this by namespace if they have chosen one?
+		meshName, namespace, err := meshutil.ChooseMesh(opts.Cache.NsResources)
+		if err != nil {
+			return fmt.Errorf("input error")
+		}
+		oMeshRef.Name = meshName
+		oMeshRef.Namespace = namespace
+	} else {
+		if !common.Contains(opts.Cache.NsResources[oMeshRef.Namespace].Meshes, oMeshRef.Name) {
+			return fmt.Errorf("Please specify a valid mesh name. Mesh %v not found in namespace %v not found", oMeshRef.Name, oMeshRef.Namespace)
+		}
+	}
+
+	return nil
 }
