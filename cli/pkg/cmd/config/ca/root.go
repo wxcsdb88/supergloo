343798f367c36be64bb85b2ed88491b051f6691b
Mitch Kelley <mitch.kelley@solo.io>
2018-11-23 14:43:32 -0500

    write the updated resource spec

--- cli/pkg/cmd/config/ca/root.go
+++ cli/pkg/cmd/config/ca/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"github.com/solo-io/supergloo/cli/pkg/cmd/options"
 	"github.com/solo-io/supergloo/cli/pkg/common"
 	"github.com/solo-io/supergloo/cli/pkg/nsutil"
@@ -17,7 +18,11 @@ func Cmd(opts *options.Options) *cobra.Command {
 		Short: `Update CA`,
 		Long:  `Update CA`,
 		Run: func(c *cobra.Command, args []string) {
-			configureCa(opts)
+			err := configureCa(opts)
+			// TODO pass err upwards
+			if err != nil {
+				fmt.Println(err)
+			}
 		},
 	}
 
@@ -34,33 +39,40 @@ func Cmd(opts *options.Options) *cobra.Command {
 	return cmd
 }
 
-func configureCa(opts *options.Options) {
-	fmt.Println(opts.Top.Static)
+func configureCa(opts *options.Options) error {
 
-	// TODO(mitchdraft) - needed for basic usage:
-	// - submit crd updates
-
+	// validate flags and gather interactive fields
 	err := ensureFlags(opts)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
-	fmt.Println("wip")
+
 	meshClient, err := common.GetMeshClient()
 	if err != nil {
-		fmt.Println(err)
-		return
-		// return err
+		return err
 	}
 	mesh, err := (*meshClient).Read(opts.Config.Ca.Mesh.Namespace, opts.Config.Ca.Mesh.Name, clients.ReadOpts{})
 	if err != nil {
-		fmt.Println(err)
-		return
-		// return err
+		return err
 	}
-	fmt.Println(mesh)
+
+	if !mesh.Encryption.TlsEnabled {
+		return fmt.Errorf("TLS is not enabled on mesh %v. You must first enable TLS before configuring CA.", opts.Config.Ca.Mesh.Name)
+	}
+
+	// TODO(mitchdraft) replace options.ResourceRef with core.ResourceRef
+	// *(mesh.Encryption.Secret) = opts.Config.Ca.Secret
+	mesh.Encryption.Secret = &core.ResourceRef{}
+	mesh.Encryption.Secret.Name = opts.Config.Ca.Secret.Name
+	mesh.Encryption.Secret.Namespace = opts.Config.Ca.Secret.Namespace
+
+	_, err := (*meshClient).Write(mesh, clients.WriteOpts{OverwriteExisting: true})
+	if err != nil {
+		return err
+	}
+
 	fmt.Printf("Configured mesh %v to use secret %v", opts.Config.Ca.Mesh.Name, opts.Config.Ca.Secret.Name)
-	return
+	return nil
 }
 
 func ensureFlags(opts *options.Options) error {
