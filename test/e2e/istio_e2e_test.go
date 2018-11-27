df49e0f488e77e193345ce640158f7ecefbce3a4
Rick Ducott <rick.ducott@solo.io>
2018-11-27 18:51:32 -0500

    Add logging

--- test/e2e/istio_e2e_test.go
+++ test/e2e/istio_e2e_test.go
@@ -199,7 +199,7 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 			return bookinfons
 		}
 
-		It("Should install istio and enable policy", func() {
+		FIt("Should install istio and enable policy", func() {
 
 			// start discovery
 			cmd := exec.Command(PathToUds, "-udsonly")
