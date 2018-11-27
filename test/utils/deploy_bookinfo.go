ecb78e2dda49786415b84d9b5e5303dd69446f29
Scott Weiss <sdw35@cornell.edu>
2018-11-27 08:54:27 -0500

    testing: support injection for deployment yamls, deploy test runner from yaml
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- test/utils/deploy_bookinfo.go
+++ test/utils/deploy_bookinfo.go
@@ -1,36 +1,11 @@
 package utils
 
 import (
-	"github.com/solo-io/supergloo/pkg/install/shared"
-	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 func DeployBookinfo(cfg *rest.Config, namespace string) error {
-	kube, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		return err
-	}
-
-	apiext, err := clientset.NewForConfig(cfg)
-	if err != nil {
-		return err
-	}
-
-	installer := shared.NewKubeInstaller(kube, apiext, namespace)
-
-	kubeObjs, err := shared.ParseKubeManifest(IstioBookinfoYaml)
-	if err != nil {
-		return err
-	}
-
-	for _, kubeOjb := range kubeObjs {
-		if err := installer.Create(kubeOjb); err != nil {
-			return err
-		}
-	}
-	return nil
+	return DeployFromYamlWithInject(cfg, namespace, IstioBookinfoYaml)
 }
 
 const IstioBookinfoYaml = `# source: istio-1.0.3/samples/bookinfo/platform/kube/bookinfo.yaml
