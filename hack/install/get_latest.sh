429350dc57a1e4f8603a7e25c767992b8d192c88
EItanya <eitan.yarmush@solo.io>
2018-11-28 11:11:17 -0500

    Updated script

--- hack/install/get_latest.sh
+++ hack/install/get_latest.sh
@@ -6,15 +6,10 @@ set -ex
 # Expects supergloo to be installed in the standard place
 SUPERGLOO_DIR="${GOPATH}/src/github.com/solo-io/supergloo"
 HACK_DIR="${SUPERGLOO_DIR}/hack/install"
-TMP_YML="${HACK_DIR}/supergloo.tmp.yml"
 
 cd $SUPERGLOO_DIR
 
 sh $HACK_DIR/recompile.sh
 
-sed 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' $HACK_DIR/supergloo.yaml > $TMP_YML
-kubectl apply -f $TMP_YML
+sed 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' $HACK_DIR/supergloo.yaml | kubectl apply -f -
 make install-cli
-
-rm $TMP_YML
-
