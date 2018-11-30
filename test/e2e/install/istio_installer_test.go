057add108fbd884a45542315174bdc8b280494aa
Rick Ducott <rick.ducott@solo.io>
2018-11-30 16:45:43 -0500

    Clean up after testing new chart

--- test/e2e/install/istio_installer_test.go
+++ test/e2e/install/istio_installer_test.go
@@ -2,7 +2,7 @@ package install
 
 import (
 	"github.com/solo-io/solo-kit/test/helpers"
-	//"github.com/solo-io/supergloo/pkg/constants"
+	"github.com/solo-io/supergloo/pkg/constants"
 
 	"github.com/solo-io/supergloo/pkg/api/v1"
 	"github.com/solo-io/supergloo/pkg/install/istio"
@@ -28,8 +28,7 @@ var _ = Describe("Istio Installer", func() {
 	}
 
 	BeforeEach(func() {
-		ChartPath = "/Users/rick/code/src/github.com/solo-io/supergloo/hack/install/istio/istio-1.0.3/istio-1.0.3.tgz"
-		//ChartPath = constants.IstioInstallPath
+		ChartPath = constants.IstioInstallPath
 		randStr := helpers.RandString(8)
 		InstallNamespace = "istio-install-test-" + randStr
 		MeshName = "istio-mesh-test-" + randStr
@@ -40,7 +39,7 @@ var _ = Describe("Istio Installer", func() {
 		util.DeleteCrb(istio.CrbName)
 	})
 
-	FIt("Can install and uninstall istio", func() {
+	It("Can install and uninstall istio", func() {
 		InstallAndWaitForPods(getInstall(true), 9)
 		UninstallAndWaitForCleanup(getInstall(false))
 
